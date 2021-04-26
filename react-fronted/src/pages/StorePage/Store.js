166ac35267bcf91ba8f96cf24c0ac8bed01cc86d
HsiehMF <mowli.0509@gamil.com>
2021-04-27 02:50:15 +0800

    StorePage: update products[] to stores[] due to naming error, and complete print data

--- react-fronted/src/pages/StorePage/Store.js
+++ react-fronted/src/pages/StorePage/Store.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import axios from 'axios';
-import styles from './index.module.scss';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import IconButton from '@material-ui/core/IconButton';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import { Link } from 'react-router-dom';
 
 export default class Store extends React.Component {
     state = {
-        products: []
+        stores: []
     }
 
     componentDidMount() {
@@ -18,14 +24,34 @@ export default class Store extends React.Component {
             })
         })
         promise.then(response => {
-            this.setState({ products: response.data });
+            this.setState({ stores: response.data });
         })
     }
 
     render() {
         return (
             <React.Fragment>
-                Test
+                {this.state.stores.map(store => (
+                    <Card style={{ maxWidth: '45%' }} >
+                        <CardHeader
+                            title={store.name}
+                            subheader={store.address}
+                            action={
+                                <IconButton aria-label="facebook">
+                                    <a href={store.link} target="_blank" rel="noopener noreferrer"><FacebookIcon /></a>
+                                </IconButton>
+                            }
+                        />                        
+                        <CardContent>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                <p>{store.description}</p>
+                                <p>創意料理：{store.cooking_name}<span className="text-danger mx-2">${store.price}</span></p>
+                                <p>店家電話：{store.tel}</p>
+                            </Typography>
+                        </CardContent>
+                    </Card>                       
+                ))}
+          
             </React.Fragment>
         )
     }
